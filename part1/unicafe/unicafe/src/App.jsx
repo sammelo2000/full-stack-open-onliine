662001d9702a3891d35f8d3b6b4546e41b69955c
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2023-12-24 15:52:33 +0200

    Exercise 1.7 unicafe step2

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -5,17 +5,78 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [totalFeedBack,setTotalFeedBack] = useState(0)
+  const [avgFeed, setAvgFeed] = useState(0)
+
+
+  let  positivePercent = ((good/totalFeedBack) * 100) 
+  positivePercent = (positivePercent)?  positivePercent : '...'
+
+  
+  
+  const calAvgFeedBack = (good,bad,total) => {
+    let goodScore = good * 1
+    let badScore = bad * -1
+    
+    console.log('scores',goodScore,badScore)
+
+    let avgFeedBack = (goodScore + badScore) / total
+    console.log('avg',avgFeedBack,'total',total)
+    
+    return avgFeedBack
+     
+  }
+  const handleSetGood = () => {
+    const updatedGood = good + 1
+    
+    const updatedTotalFeedBack = updatedGood + bad + neutral
+    console.log("updatedGood:",updatedGood,"updatedTotalFeedback",updatedTotalFeedBack)
+    setGood(updatedGood)
+    setTotalFeedBack(updatedTotalFeedBack)
+    
+    
+    
+    
+    setAvgFeed(calAvgFeedBack(updatedGood,bad,updatedTotalFeedBack))
+    
+   
+    
+  } 
+
+  const handleSetBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedTotalFeedBack = updatedBad + good + neutral
+    setTotalFeedBack(updatedTotalFeedBack)
+    console.log(updatedBad)
+    setAvgFeed(calAvgFeedBack(good,updatedBad,updatedTotalFeedBack))
+  }
+
+  const handleSetNuetral = () => {
+    const updatedNuetral = neutral + 1
+    setNeutral(updatedNuetral)
+    const updatedTotalFeedBack = updatedNuetral + good + bad
+    setTotalFeedBack(updatedTotalFeedBack)
+    console.log(updatedNuetral)
+    setAvgFeed(calAvgFeedBack(good,bad,updatedTotalFeedBack))
+  }
+
+  
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={handleSetGood}>good</button>
+      <button onClick={handleSetNuetral}>neutral</button>
+      <button onClick={handleSetBad}>bad</button>
+
       <h2>statistics</h2>
       <p>good: {good} </p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad} </p>
+      <p>Total: {totalFeedBack}</p>
+      <p>Average: {avgFeed}</p>
+      <p>Postive Feedback: {positivePercent}%</p>
     </div>
   )
 }

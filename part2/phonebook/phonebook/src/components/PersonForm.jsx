2e6e6faf86fa969a63ae3d70b9af64620a2d3413
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2023-12-29 15:07:06 +0200

    Personform component made

---
+++ part2/phonebook/phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const PersonForm = (props) => {
+    console.log(props)
+    const {
+        handleAddContact,
+        newName,
+        handleNameChange,
+        newNumber,
+        handleNumberChange
+    } = props
+    
+  return (
+    <div>
+        <form onSubmit={handleAddContact}>
+        <div>
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+            
+          />
+        </div>
+        <div>
+          number: 
+          <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default PersonForm

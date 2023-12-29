10d3e8235e75bf07597947c800172aa39976801c
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2023-12-29 14:04:51 +0200

    fillter contact logic

--- part2/phonebook/phonebook/src/App.jsx
+++ part2/phonebook/phonebook/src/App.jsx
@@ -3,10 +3,27 @@ import ContactList from "./components/ContactList"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
+  const [fillteredName,setFillteredName] = useState('')
+  const [fillteredResult,setFillteredResult] = useState([])
+  
+
+  const handleFilterContact = (e) => {
+      console.log('handleFilter',e.target)
+      console.log('Target Value:', e.target.value)
+      const findName = e.target.value
+      setFillteredName(e.target.value)
+      const fillteredNames = persons.filter(person =>
+         person.name.toLocaleLowerCase() === findName.toLocaleLowerCase() )
+    console.log('Filltered Result ', fillteredNames)
+    setFillteredResult(fillteredNames)
+  } 
 
 
   const handleNameChange = (e) => {
@@ -58,11 +75,16 @@ const App = () => {
 
   }
 
-
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
+       <>Search Contact:<input
+        value={fillteredName}
+        onChange={handleFilterContact}
+       /></>
+      <h2>add a new contact</h2>
       <form onSubmit={handleAddContact}>
         <div>
           name: 
@@ -88,7 +110,7 @@ const App = () => {
       <ul>
         {
           persons.map(person =>
-            <ContactList key={person.name} contact={person} />
+            <ContactList key={person.id} contact={person} />
             )
         }
       </ul>

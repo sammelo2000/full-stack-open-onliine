309038eb6a01a36fede36c017d5afccb62ef147f
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2024-01-05 20:56:38 +0200

    Exercise 2.13 phonebook step8

--- part2/phonebook/phonebook/src/components/contactService.jsx
+++ part2/phonebook/phonebook/src/components/contactService.jsx
@@ -10,22 +10,9 @@ const getAll = () => {
                     
 }
 
-const create = () => {
-    axios
-    .post(baseUrl,newContact)
-    .then(response => {
-      const newContacts = persons.concat(response.data)
-      console.log('New Contacts list',newContacts)
-      setPersons(newContacts)
-      setNewName('')
-      setNewNumber('')
-
-      console.log('Contact Added')
-      console.log(newContacts)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+const create = (newContact) => {
+  const request = axios.post(baseUrl,newContact)
+  return request
 }
 
 export default {getAll,create}
